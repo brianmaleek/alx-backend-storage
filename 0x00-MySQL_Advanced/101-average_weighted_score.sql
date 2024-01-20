7982336516ccc3f5bbb61ae18e301fcf9f97ea13
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-01-20 21:14:28 +0300

    updated

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -4,24 +4,13 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE total_weighted_score FLOAT;
-    DECLARE total_weights FLOAT;
-
-    -- Calculate total weighted score and total weights for all users
-    SELECT
-        SUM(corrections.score * projects.weight),
-        SUM(projects.weight)
-    INTO
-        total_weighted_score,
-        total_weights
-    FROM
-        corrections
-    JOIN
-        projects ON corrections.project_id = projects.id;
-
-    -- Update average_score for all users
     UPDATE users
-    SET average_score = IFNULL(total_weighted_score / NULLIF(total_weights, 0), 0);
+    SET average_score = (
+        SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
+        FROM corrections
+        JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = users.id
+    );
 END //
 
 DELIMITER ;
